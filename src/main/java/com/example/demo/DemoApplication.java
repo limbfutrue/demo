1c9ff2d481eca161a6ec5471171ba71f5e545685
libaoming <13716572013@163.com>
2019-01-21 10:38:37 +0800

    添加程序入口注释

--- src/main/java/com/example/demo/DemoApplication.java
+++ src/main/java/com/example/demo/DemoApplication.java
@@ -7,7 +7,7 @@ import org.springframework.context.annotation.ComponentScan;
 
 @SpringBootApplication(exclude = DataSourceAutoConfiguration.class)
 public class DemoApplication {
-
+	//程序入口
 	public static void main(String[] args) {
 		SpringApplication.run(DemoApplication.class, args);
 	}
